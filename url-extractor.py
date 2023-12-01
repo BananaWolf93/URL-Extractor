4df2bbbb05767a8beb6f348c1d11d37c5d55ac60
BananaWolf93 <christopherkentleal@gmail.com>
2023-11-30 17:15:26 -0700

    Added support for user input for URL ingestion

--- url-extractor.py
+++ url-extractor.py
@@ -4,20 +4,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Variables
-url = 'https://google.com'
-response = requests.get(url)
+# Variables designed to accept a user's input and store the URL in the "userinput" variable which is then passed to requests.
+userinput = input("[Enter the URL]: ")
+response = requests.get(userinput)
 
 
-# Saving a GET request with "requests" into the variable "content":
+# Saving a GET request with "requests" into the variable "content" and only accepting successful 200 status codes.:
 if response.status_code == 200:
     content = response.content
     print("Successful [200] response")
-    
-else:
-    print("Error: {response.status_code}")
 
-soup = BeautifulSoup(content, 'html.parser')
+# beautiful soup then parses the data from the "content" variable and extracts the URLs found within the response.
 
-for link in soup.find_all('a'):
+    soup = BeautifulSoup(content, 'html.parser')
+
+    for link in soup.find_all('a'):
         print(link.get('href'))
+
+else:
+    print("Error: ", (response.status_code))
